8d188dd65267411002fa9816434d8f7b651acfc8
Sohad Dader <sohad@leannode.com>
2019-12-01 10:50:23 +0200

    add components

---
+++ components/CategoryGridTile.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { TouchableOpacity, View, Text, StyleSheet } from 'react-native';
+
+const CategoryGridTile = props => {
+  return (
+    <TouchableOpacity style={styles.gridItem} onPress={props.onSelect}>
+      <View
+        style={{ ...styles.container, ...{ backgroundColor: props.color } }}
+      >
+        <Text style={styles.title} numberOfLines={2}>
+          {props.title}
+        </Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  gridItem: {
+    flex: 1,
+    margin: 15,
+    height: 150,
+    borderRadius: 10,
+    overflow: 'hidden'
+  },
+  container: {
+    flex: 1,
+    borderRadius: 10,
+    shadowColor: 'black',
+    shadowOpacity: 0.26,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 10,
+    elevation: 3,
+    padding: 15,
+    justifyContent: 'flex-end',
+    alignItems: 'flex-end'
+  },
+  title: {
+    fontFamily: 'open-sans-bold',
+    fontSize: 22,
+    textAlign: 'right'
+  }
+});
+
+export default CategoryGridTile;
