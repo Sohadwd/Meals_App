cf40f75fc7b50fa374c1b0f8101f0f100677e5e1
Sohad Dader <sohad@leannode.com>
2019-12-01 10:49:25 +0200

    add header

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,15 +1,29 @@
 import { createStackNavigator, createAppContainer } from 'react-navigation';
+import { Platform } from 'react-native';
+import Colors from '../constants/Colors';
 
 import CategoriesScreen from '../screens/CategoriesScreen';
 import CategoryMealsScreen from '../screens/CategoryMealsScreen';
 import MealDetailScreen from '../screens/MealDetailScreen';
 
-const MealsNavigator = createStackNavigator({
-  Categories: CategoriesScreen,
-  CategoryMeals: {
-    screen: CategoryMealsScreen
+const MealsNavigator = createStackNavigator(
+  {
+    Categories: {
+      screen: CategoriesScreen
+    },
+    CategoryMeals: {
+      screen: CategoryMealsScreen
+    },
+    MealDetail: MealDetailScreen
   },
-  MealDetail: MealDetailScreen
-});
+  {
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : ''
+      },
+      headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primaryColor
+    }
+  }
+);
 
 export default createAppContainer(MealsNavigator);
