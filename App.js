4a7c6d69e45c78a8860a9810bfe21e16f2c1450c
Sohad Dader <sohad@leannode.com>
2019-12-01 10:49:44 +0200

    add usescreen

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
-import { Text, View } from 'react-native';
 import * as Font from 'expo-font';
 import { AppLoading } from 'expo';
+import { useScreens } from 'react-native-screens';
 
 import MealsNavigator from './navigation/MealsNavigator';
+
+useScreens();
 
 const fetchFonts = () => {
   return Font.loadAsync({
