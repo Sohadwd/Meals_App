2862f8a2827354c4282b0fba285b9f0e284cd0dc
Sohad Dader <sohad@leannode.com>
2019-12-05 12:19:31 +0200

    use our text component

--- components/MealItem.js
+++ components/MealItem.js
@@ -7,6 +7,8 @@ import {
   ImageBackground
 } from 'react-native';
 
+import DefaultText from './DefaultText';
+
 const MealItem = props => {
   return (
     <View style={styles.mealItem}>
@@ -22,9 +24,9 @@ const MealItem = props => {
         </View>
 
         <View style={{ ...styles.mealRow, ...styles.mealDetail }}>
-          <Text>{props.duration}m</Text>
-          <Text>{props.complexity.toUpperCase()}</Text>
-          <Text>{props.affordability.toUpperCase()}</Text>
+          <DefaultText>{props.duration}m</DefaultText>
+          <DefaultText>{props.complexity.toUpperCase()}</DefaultText>
+          <DefaultText>{props.affordability.toUpperCase()}</DefaultText>
         </View>
       </TouchableOpacity>
     </View>
