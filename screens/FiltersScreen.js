a03631cc9601baad568ef5bd46c84e349b431206
Sohad Dader <sohad@leannode.com>
2019-12-05 11:58:57 +0200

    add filter screen

--- screens/FiltersScreen.js
+++ screens/FiltersScreen.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+
+import HeaderButton from '../components/HeaderButton';
 
 const FiltersScreen = props => {
   return (
@@ -7,6 +10,23 @@ const FiltersScreen = props => {
       <Text>The Filters Screen!</Text>
     </View>
   );
+};
+
+FiltersScreen.navigationOptions = navData => {
+  return {
+    headerTitle: 'Filter Meals',
+    headerLeft: (
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item
+          title="Menu"
+          iconName="ios-menu"
+          onPress={() => {
+            navData.navigation.toggleDrawer();
+          }}
+        />
+      </HeaderButtons>
+    )
+  };
 };
 
 const styles = StyleSheet.create({
