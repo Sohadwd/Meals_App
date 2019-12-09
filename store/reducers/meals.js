20d6916dab5a0ddcd5e021959cc4c4bc527b2bad
Sohad Dader <sohad@leannode.com>
2019-12-09 14:15:30 +0200

    add action fav to reducer

--- store/reducers/meals.js
+++ store/reducers/meals.js
@@ -1,4 +1,5 @@
 import { MEALS } from '../../data/dummy-data';
+import { TOGGLE_FAVORITE } from '../actions/meals';
 
 const initialState = {
   meals: MEALS,
@@ -7,7 +8,23 @@ const initialState = {
 };
 
 const mealsReducer = (state = initialState, action) => {
-  return state;
+  console.log('action', action);
+  switch (action.type) {
+    case TOGGLE_FAVORITE:
+      const existingIndex = state.favoriteMeals.findIndex(
+        meal => meal.id === action.mealId
+      );
+      if (existingIndex >= 0) {
+        const updatedFavMeals = [...state.favoriteMeals];
+        updatedFavMeals.splice(existingIndex, 1);
+        return { ...state, favoriteMeals: updatedFavMeals };
+      } else {
+        const meal = state.meals.find(meal => meal.id === action.mealId);
+        return { ...state, favoriteMeals: state.favoriteMeals.concat(meal) };
+      }
+    default:
+      return state;
+  }
 };
 
 export default mealsReducer;
